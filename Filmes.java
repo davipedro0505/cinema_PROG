65879fc3cb90245a4062a1f8bd938fb3a91ee428
Laís Santos <lss52@discente.ifpe.edu.br>
2025-12-09 15:58:38 -0300

    Refactor Filmes class into Filme

--- Filmes.java
+++ Filmes.java
@@ -1,59 +1,71 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Filmes {
+public class Filme {
 
-private String titulo;
-private String genero;
-private String duracao;
-private List<Ator> atores;
+    private String titulo;
+    private String genero;
+    private String duracao;
+    private List<Ator> atores;
 
-public Filmes(String titulo,String genero,String duracao){
-    this.titulo = titulo;
-    this.genero = genero;
-    this.duracao = duracao;
-    this.atores = new ArrayList<>();
-}
- public String getTitulo(){
-    return titulo;
- }
- public void setTitulo (String titulo){
-    this.titulo = titulo;
- } 
- public String getGenero(){
-    return genero; 
-}
- public void setGenero(String genero){
-    this.genero = genero;
- }
- public String getDuracao(){
-    return duracao;
- }
- public void setDuracao(String duracao){
-    this.duracao = duracao;
- }
- public List<Ator> getAtores(){
-    return atores;
- }
- public void setAtores(List<Ator> atores){
-    this.atores = atores;
- }
- public void adicionarAtor(Ator a){
-    atores.add(a);
- }
- public void removerAtor(Ator a){
-    atores.remove(a);
- }
- public void listarAtores(){
-    System.out.println("Atores do filme'" + titulo + "':");
-    for (Ator a : atores){
-        System.out.println("-" + a.getNome() + "(" + a.getIdade() + "anos)");
+    public Filme(String titulo, String genero, String duracao) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracao = duracao;
+        this.atores = new ArrayList<>();
     }
- }
- public void imprimirInf(){
-    System.out.println("Titulo: " + titulo);
-    System.out.println("Gênero: " + genero);
-    System.out.println("duração: " + duracao);
-    System.out.println("Quantidade de atores:" + atores.size());
- }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(String duracao) {
+        this.duracao = duracao;
+    }
+
+    public List<Ator> getAtores() {
+        return atores;
+    }
+
+    public void setAtores(List<Ator> atores) {
+        this.atores = atores;
+    }
+
+    public void adicionarAtor(Ator ator) {
+        atores.add(ator);
+    }
+
+    public void removerAtor(Ator ator) {
+        atores.remove(ator);
+    }
+
+    public void listarAtores() {
+        System.out.println("Atores do filme '" + titulo + "':");
+        for (Ator ator : atores) {
+            System.out.println("- " + ator.getNome() + " (" + ator.getIdade() + " anos)");
+        }
+    }
+
+    public void imprimirInfo() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Gênero: " + genero);
+        System.out.println("Duração: " + duracao);
+        System.out.println("Quantidade de atores: " + atores.size());
+    }
 }
