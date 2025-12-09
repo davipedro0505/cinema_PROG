c142bbc8b93fac7f226786cdd256b28eb7c4168d
Laís Santos <lss52@discente.ifpe.edu.br>
2025-12-09 16:01:29 -0300

    Refactor Ingresso class to use Filme instead of evento

--- Ingresso.java
+++ Ingresso.java
@@ -1,22 +1,31 @@
 public class Ingresso {
-    private String evento;
-    private double preco;
-    private Cliente cliente; 
 
-    protected Ingresso(String evento, double preco, Cliente cliente) {
-        this.evento = evento;
+    private Filme filme;
+    private double preco;
+    private Cliente cliente;
+
+    protected Ingresso(Filme filme, double preco, Cliente cliente) {
+        this.filme = filme;
         this.preco = preco;
         this.cliente = cliente;
     }
 
+    public Filme getFilme() {
+        return filme;
+    }
 
-    public String getEvento() { return evento; }
-    public double getPreco() { return preco; }
-    public Cliente getCliente() { return cliente; }
+    public double getPreco() {
+        return preco;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
 
     @Override
     public String toString() {
-        return "Ingresso para " + evento + " - R$" + preco + 
+        return "Ingresso para o filme '" + filme.getTitulo() +
+               "' - R$ " + preco +
                " (Cliente: " + cliente.getNome() + ")";
     }
 }
