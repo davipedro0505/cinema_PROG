947c3ee7effd3b4e7b89ebf11c4b278916bd2039
Laís Santos <lss52@discente.ifpe.edu.br>
2025-12-09 16:00:41 -0300

    Refactor Ator class and update film handling

--- Ator.java
+++ Ator.java
@@ -1,49 +1,56 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Ator{
-private String nome;
-private int idade;
-private List<Filmes> filmes;
+public class Ator {
 
-public Ator(String nome, int idade){
-this.nome = nome;
-this.idade = idade;
-this.filmes = new ArrayList<>();
-}
+    private String nome;
+    private int idade;
+    private List<Filme> filmes;
 
-public String getNome(){
-    return nome; 
-}
-public void setNome(String nome){
-    this.nome = nome;
-}
-public int getIdade(){
-    return idade;
-}
-public void setIdade(int idade){
-    this.idade = idade;
-}
-public List<Filmes> getFilmes(){
-    return filmes;
-}
-public void setFilme(List<Filmes> filmes){
-    this.filmes = filmes;
-}
-public void adicionarFilme(Filmes f) {
-        filmes.add(f);
-}
+    public Ator(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.filmes = new ArrayList<>();
+    }
 
-public void mostrarFilme() {
+    public String getNome() {
+        return nome;
+    }
 
-    System.out.println("Ator:" + nome);
-    System.out.println("Idade:" + idade);
-    System.out.println("Filmes de " + nome + ":");
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public List<Filme> getFilmes() {
+        return filmes;
+    }
+
+    public void setFilmes(List<Filme> filmes) {
+        this.filmes = filmes;
+    }
+
+    public void adicionarFilme(Filme filme) {
+        filmes.add(filme);
+    }
+
+    public void mostrarFilmes() {
+        System.out.println("Ator: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Filmes de " + nome + ":");
+
         if (filmes.isEmpty()) {
             System.out.println("Nenhum filme cadastrado para este ator.");
         } else {
-            for (Filmes f : filmes) {
-                System.out.println("- " + f.getTitulo());
+            for (Filme filme : filmes) {
+                System.out.println("- " + filme.getTitulo());
             }
         }
     }
