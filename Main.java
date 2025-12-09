49cea3ff87a04b6796b1ddf3468f50dfa8bf20d0
Laís Santos <lss52@discente.ifpe.edu.br>
2025-12-09 16:02:33 -0300

    Refactor Filme class usage and improve method calls

--- Main.java
+++ Main.java
@@ -4,21 +4,20 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-       
         Ator ator1 = new Ator("Raphael Montes", 34);
-        Filmes filme1 = new Filmes("Dias Perfeitos", "Suspense", "2h 45min");
-        Filmes filme3 = new Filmes("A volta dos que não foram", "Comédia", "3h 10min");
+        Filme filme1 = new Filme("Dias Perfeitos", "Suspense", "2h 45min");
+        Filme filme3 = new Filme("A volta dos que não foram", "Comédia", "3h 10min");
 
         ator1.adicionarFilme(filme1);
         ator1.adicionarFilme(filme3);
-        ator1.mostrarFilme();
+        ator1.mostrarFilmes();
 
         System.out.println("\n---");
 
         Ator ator2 = new Ator("Neiff de Jesus", 27);
-        Filmes filme2 = new Filmes("Juntos para sempre", "Romance", "2h 15min");
+        Filme filme2 = new Filme("Juntos para sempre", "Romance", "2h 15min");
         filme2.adicionarAtor(ator2);
-        filme2.imprimirInf();
+        filme2.imprimirInfo();
         filme2.listarAtores();
 
         List<Cliente> listaDeClientes = new ArrayList<>();
@@ -35,8 +34,9 @@ public class Main {
             c.imprimirInfo();
         }
 
-        c1.comprarIngresso("Show da Banda X", 120.0);
-        c1.comprarIngresso("Teatro Y", 80.0);
+        // Compra de ingressos para FILMES
+        c1.comprarIngresso(filme1, 30.0);
+        c1.comprarIngresso(filme2, 28.0);
 
         System.out.println("\nIngressos de " + c1.getNome() + ":");
         for (Ingresso i : c1.listarIngressos()) {
@@ -52,9 +52,17 @@ public class Main {
         }
 
         System.out.println("\n--- Funcionário ---");
-        Funcionario f1 = new Funcionario("111.222.333-44", "Carlos Souza", "carlos@ifpe.edu.br", "Atendente", 2500.0f);
+        Funcionario f1 = new Funcionario(
+                "111.222.333-44",
+                "Carlos Souza",
+                "carlos@ifpe.edu.br",
+                "Atendente",
+                2500.0f
+        );
+
         f1.imprimirInfo();
         f1.addAumento(500.0f);
+
         System.out.println("Após aumento:");
         f1.imprimirInfo();
     }
